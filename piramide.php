354735a377d6374651dcd590fe1aa54438120196
unknown <2120367@talnet.nl>
2019-10-09 11:49:09 +0200

    Uitwerking van de exercise

---
+++ piramide.php
@@ -0,0 +1,32 @@
+<?php
+
+$getal;
+
+echo "Hoeveel stapels wil je? ";
+
+$getal = readline();
+
+echo $getal;
+
+
+for ($i=0; $i <= $getal; $i++)
+{
+  printPiramid($i);
+}
+
+
+
+function printPiramid($j)
+{
+
+  for ($i=0; $i < $j; $i++)
+  {
+    echo '*';
+  }
+
+  echo "\n";
+
+}
+
+
+ ?>
